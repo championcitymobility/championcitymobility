8dc1d295eabff7e3d588a01136a4c4bfe85dc068
championcitymobility <245913208+championcitymobility@users.noreply.github.com>
2025-11-26 19:57:04 +0300

    Add name input and conditional greetings

--- fav/setup.py
+++ fav/setup.py
@@ -1,2 +1,8 @@
 print('hello world')
-
+name = input('Enter a name: ')
+if name == 'championcitymobility':
+    print('hello champion')
+elif name == ('Member'):
+  print('Hello member')
+else:
+  print('goodbye')
